90c616e925e3afa52408ce48a1781fa678898563
ClaireBrts <claire.briantais@gmail.com>
2020-05-18 11:41:54 +0200

    ajout start

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 
-//
 
 class App extends Component {
 
   state={
-    chenillard: null
+    chenillard: ''
   }
 
   constructor(props) {
@@ -20,38 +18,54 @@ class App extends Component {
       headers: {
         "Content-type": "application/json; charset=UTF-8"
       }
-    })
-    .then(res => res.json()
-    )
-     .then(data => {
+    }).then(res => res.json())
+      .then((data) => {
         console.log(data);
         this.setState({ chenillard: data })
       })
       .catch(console.log)
   }
 
+handleResponseError(response) {
+      throw new Error("HTTP error, status = " + response.status);
+  }
+  handleError(error) {
+      console.log(error.message);
+  }
+
+async start() {
+    console.log("START DIS OUI:");
+    return fetch("rest/chenillard/getStart")
+      .then(response => {
+       if (!response.ok) {
+            this.handleResponseError(response);
+        }
+
+      })
+      .catch(error => {
+        this.handleError(error);
+      });
+  }
 
 
   render() {
     return (
       <div className="App">
-
-            <div className="ButtonStart">
-              <button type="submit" ref="buttonStart" onSubmit={this.onSubmitEvent}> Start</button>
-            </div>
-
-            <div className="ButtonStop">
-              <button type="submit" ref="buttonStop" onSubmit={this.onSubmitEvent}> Stop</button>
-            </div>
-            <div className="chenillard">
-            <title> Chenillard </title>
-            </div>
-
-
+        <div className="ButtonStart">
+          <button type="submit" onClick={()=>{this.start()}}> Start</button>
         </div>
+        <div className="ButtonStop">
+          <button type="submit" onClick={this.onSubmitEvent}> Stop</button>
+        </div>
+      <div className="chenillard">
+      <title> Chenillard </title>
+      {`Vitesse: ${this.state.chenillard.vitesse} Run: ${this.state.chenillard.run}  Sens: ${this.state.chenillard.sens}`}
+      </div>
+
+      </div>
     );
 
-}
+  }
 
 }
 
