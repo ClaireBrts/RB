b8243efe650bc111994a08610fffde7b3633b45f
ClaireBrts <claire.briantais@gmail.com>
2020-05-18 12:24:49 +0200

    ajout ralentir

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -5,111 +5,131 @@ import React, { Component } from 'react';
 class App extends Component {
 
   state = {
-    url : "/rest/chenillard/",
-    chenillard: ""
+	url : "/rest/chenillard/",
+	chenillard: ""
   }
 
   constructor(props) {
-    super(props);
+	super(props);
   }
 
   componentDidMount() {
-    this.refresh()
+	this.refresh()
   }
 
   async refresh() {
-    fetch(this.state.url +'getChenillard/', {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    }).then(res => res.json())
-      .then((data) => {
-        console.log(data);
-        this.setState({ chenillard: data })
-      })
-      .catch(error => {
-               this.handleError(error);
-             })
+	fetch(this.state.url +'getChenillard/', {
+	  method: "GET",
+	  headers: {
+		"Content-type": "application/json; charset=UTF-8"
+	  }
+	}).then(res => res.json())
+	  .then((data) => {
+		console.log(data);
+		this.setState({ chenillard: data })
+	  })
+	  .catch(error => {
+			   this.handleError(error);
+			 })
   }
 
   async stop() {
-    console.log("Stop");
-    return fetch(this.state.url +'getStop/')
-      .then(response => {
-        if (!response.ok) {
-          this.handleResponseError(response);
-        }
-        else {
-          this.refresh()
-        }
-      })
-      .catch(error => {
-        this.handleError(error);
-      });
+	console.log("Stop");
+	return fetch(this.state.url +'getStop/')
+	  .then(response => {
+		if (!response.ok) {
+		  this.handleResponseError(response);
+		}
+		else {
+		  this.refresh()
+		}
+	  })
+	  .catch(error => {
+		this.handleError(error);
+	  });
 
   }
 
   async start() {
-    console.log("START:");
-    return fetch(this.state.url +"getStart")
-      .then(response => {
-        if (!response.ok) {
-          this.handleResponseError(response);
-        }
-        else {
-          this.refresh()
-        }
-      })
-      .catch(error => {
-        this.handleError(error);
-      });
+	console.log("START:");
+	return fetch(this.state.url +"getStart")
+	  .then(response => {
+		if (!response.ok) {
+		  this.handleResponseError(response);
+		}
+		else {
+		  this.refresh()
+		}
+	  })
+	  .catch(error => {
+		this.handleError(error);
+	  });
   }
 
   async changeSens() {
-      console.log("ChangeSens");
-      return fetch(this.state.url +"getChangeSens")
-        .then(response => {
-         if (!response.ok) {
-              this.handleResponseError(response);
-          }
-          else {
-                    this.refresh()
-           }
+	  console.log("ChangeSens");
+	  return fetch(this.state.url +"getChangeSens")
+		.then(response => {
+		 if (!response.ok) {
+			  this.handleResponseError(response);
+		  }
+		  else {
+			this.refresh()
+		   }
 
-        })
-        .catch(error => {
-          this.handleError(error);
-        });
-    }
+		})
+		.catch(error => {
+		  this.handleError(error);
+		});
+	}
+
+	async ralentir() {
+    	  console.log("Ralentir");
+    	  return fetch(this.state.url +"getRalentir")
+    		.then(response => {
+    		 if (!response.ok) {
+    			  this.handleResponseError(response);
+    		  }
+    		  else {
+    			this.refresh()
+    		   }
+
+    		})
+    		.catch(error => {
+    		  this.handleError(error);
+    		});
+    	}
 
 handleResponseError(response) {
-    throw new Error("HTTP error, status = " + response.status);
+	throw new Error("HTTP error, status = " + response.status);
 }
 handleError(error) {
-    console.log(error.message);
+	console.log(error.message);
 }
 
   render() {
-    return (
-      <div className="App">
+	return (
+	  <div className="App">
 
-        <div className="ButtonStart">
-          <button type="submit" onClick={()=>{this.start()}}> Start</button>
-        </div>
-        <div className="ButtonStop">
-          <button onClick={()=>{this.stop()}}> Stop</button>
-        </div>
-        <div className="ButtonSens">
-          <button onClick={()=>{this.changeSens()}}> Changer de Sens</button>
-        </div>
-        <div className="chenillard">
-          <title> Chenillard </title>
-          {`Vitesse: ${this.state.chenillard.vitesse} Run: ${this.state.chenillard.run}  Sens: ${this.state.chenillard.sens}`}
-        </div>
+		<div className="ButtonStart">
+		  <button type="submit" onClick={()=>{this.start()}}> Start</button>
+		</div>
+		<div className="ButtonStop">
+		  <button onClick={()=>{this.stop()}}> Stop</button>
+		</div>
+		<div className="ButtonSens">
+		  <button onClick={()=>{this.changeSens()}}> Changer de Sens</button>
+		</div>
+		<div className="ButtonSens">
+        		  <button onClick={()=>{this.ralentir()}}>Ralentir</button>
+        		</div>
+		<div className="chenillard">
+		  <title> Chenillard </title>
+		  {`Vitesse: ${this.state.chenillard.vitesse} Run: ${this.state.chenillard.run}  Sens: ${this.state.chenillard.sens}`}
+		</div>
 
-      </div>
-    );
+	  </div>
+	);
 
   }
 
